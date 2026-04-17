c1caed5b65dc1c8ee564041aad5776b3adffe127
akulaakash17-byte <akulaakash17@gmail.com>
2026-04-17 23:16:14 +0530

    updated db for production

--- db.js
+++ db.js
@@ -1,13 +1,24 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT),
-});
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: parseInt(process.env.DB_PORT),
+      }
+);
 
 pool.on("connect", () => {
   console.log("✅ PostgreSQL connected");
@@ -17,4 +28,4 @@ pool.on("error", (err) => {
   console.error("❌ PostgreSQL error:", err.message);
 });
 
-module.exports = pool; 
+module.exports = pool;
